34f64869c032e483b71c8ec04a7ea65001b4086c
Theo Browne <me@t3.gg>
2025-02-15 18:03:21 -0800

    pwa because why not

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
+  ],
+});
