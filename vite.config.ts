cf3b5428380c704c5ff103e8f001e9081e6de40f
Zach Teisberg <zteisberg@gmail.com>
2025-02-20 08:54:59 -0800

    Move build to docs folder for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  build: {
+    outDir: '../docs',
+    emptyOutDir: true
+  },
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
